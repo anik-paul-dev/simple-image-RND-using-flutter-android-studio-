55fd9b4f1996af46b3072bcbf424c5f97939955d
anik-paul-dev <anikpaul.dev24@gmail.com>
2025-01-30 17:29:45 +0600

    image rnd first committ

---
+++ android/app/src/main/kotlin/com/example/flutter_imagernd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_imagernd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( Image_13_demo());
+}
+//
+// class Image_13_demo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//             appBar: AppBar(
+//               title: Text('Flutter Image Demo'),
+//             ),
+//             body: Center(
+//               child: Column(
+//                 children: <Widget>[
+//
+//                   Image.asset('assets/anik_paul.png'),
+//                   Text(
+//                     'Back image',
+//                     style: TextStyle(fontSize: 20.0),
+//                   )
+//                 ],
+//               ),
+//             ),
+//             ),
+//         );
+//   }
+// }
+
+//Display images from the internet
+
+
+class Image_13_demo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Image Demo'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                  'https://picsum.photos/250?image=9',
+
+                  height: 400,
+                  width: 250
+              ),
+              Text(
+                'It is an image displays from the given url.',
+                style: TextStyle(fontSize: 20.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
